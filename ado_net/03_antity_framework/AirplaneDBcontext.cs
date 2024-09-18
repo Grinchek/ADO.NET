c8da8c5ce09476a086ecdf9792c5c4ea372620b6
Grinchek <warning686@gmail.com>
2024-09-18 17:55:33 +0300

    entity framework first lesson

---
+++ ado_net/03_antity_framework/AirplaneDBcontext.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _03_antity_framework
+{
+    public class AirplaneDBcontext:DbContext
+    {
+        //Collections
+        //Airplane
+        //Customers
+        //Flight
+        public AirplaneDBcontext() 
+        {
+            this.Database.EnsureDeleted();
+            this.Database.EnsureCreated();
+        }
+ 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"workstation id=Shomiakom3.mssql.somee.com;packet size=4096;user id=Grincchik_SQLLogin_1;pwd=gstgjnpeqp;data source=Shomiakom3.mssql.somee.com;persist security info=False;initial catalog=Shomiakom3;TrustServerCertificate=True");
+        }
+    }
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Artist> Artists { get; set; }
+
+    }
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //RelationShip type Many to Many (*...*)
+        public int CountryId { get; set; }
+        public Country Country { get; set; }    
+        public ICollection<Semple> Semples { get; set; }
+        public ICollection<Album> Album { get; set; }
+    }
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Release { get; set; }
+        //RelationShip type One to Many (1.....*)
+        public int GenreId { get; set; }
+        public Genre Genrre { get; set; }
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+        public ICollection<Semple> Semples { get; set; }
+        
+
+    }
+    public class PlayList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ICollection<Semple> Semples { get; set; }
+    }
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Semple
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+        public int AlbumId { get; set; }
+        public Album Album { get; set; }
+        public ICollection<PlayList> PlayLists { get; set; }
+
+    }
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ado_net/03_antity_framework/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03_antity_framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AirplaneDBcontext dBcontext=new AirplaneDBcontext();
+            dBcontext.Customers.Add(new Customer 
+            {
+                Name="Maxim",
+                Birthdate=new DateTime(2000,12,15),
+                Email=  "max@gmail.com"
+            });
+            dBcontext.SaveChanges();
+            foreach (var item in dBcontext.Customers)
+            {
+                Console.WriteLine($"Name : {item.Name}");
+            }
+        }
+    }
+}
